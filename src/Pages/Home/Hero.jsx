a4bd484f0341a582df3ddea6bdf5c9210a97d1a6
Mizanur Rahman <mizanrxb@gmail.com>
2023-05-02 23:04:35 +0600

    title svg hero section added

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Lottie from "lottie-react";
+import OrderPrepare from "../../../public/OrderPrepare.json"
 
 const Hero = () => {
-    return (
-        <div>
-            <h3>this is hero section</h3>
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container flex flex-col items-center my-container md:flex-row lg:py-2">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-3xl font-medium text-orange-500 title-font sm:text-4xl md:text-4xl md:leading-normal">
+          A Chef's Journey
+            <br className="hidden lg:inline-block" />
+            Through Culinary Delights
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          is a website that takes you on a culinary adventure through the eyes of a professional chef. This platform is an ode to the art of cooking and a celebration of the diversity and richness of cuisine from around the world. Follow the chef's journey as they explore various ingredients,
+          </p>
+          
         </div>
-    );
+        <div className="w-5/6 -z-50 lg:max-w-lg lg:w-7/12 md:w-1/2">
+          <div className="-mt-16 -z-50"><Lottie animationData={OrderPrepare} loop={true} /></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,11 +1,102 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
-    return (
-        <div>
-            <h3>header section</h3>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="relative flex items-center justify-between">
+        {/* Logo Section */}
+        <Link to="/" className="inline-flex items-center flex-1">
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+            Kitchen Chronicles
+          </span>
+        </Link>
+
+        {/* Nav Items Section */}
+        
+        <ul className="items-center hidden space-x-8 lg:flex">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+        <img
+          className="w-8 h-8 mr-5 rounded-full lg:ml-7"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+        {/* Mobile Navbar Section */}
+        <div className="lg:hidden">
+          {/* Dropdown Open Button */}
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className="w-5 text-gray-600" />
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 z-10 w-full">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                {/* Logo & Button section */}
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <Link to="/" className="inline-flex items-center">
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        Kitchen Chronicles
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className="w-5 text-gray-600" />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <Link to="/" className="default">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/blog"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
