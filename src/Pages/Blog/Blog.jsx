6e0a68c080d6b43d7962e16b8c1a687c05cfce3c
Mizanur Rahman <mizanrxb@gmail.com>
2023-05-05 11:14:19 +0600

    pdf generator added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,36 +1,67 @@
-import React from 'react';
+import React from "react";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const Blog = () => {
-    return (
-        <div className="my-container lg:w-2/4">
-      <div className="p-8 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
-        <h1 className="my-5 text-2xl font-semibold">
-        Differences between uncontrolled and controlled components
-        </h1>
-        <p className="font-normal">
-        Controlled components use React state to manage and update their values, while uncontrolled components rely on the browser's default behavior and the DOM API. Controlled components offer more control and flexibility, while uncontrolled components are simpler to set up.
-        </p>
+  return (
+    <section>
+      <div className="flex justify-center">
+        <Pdf targetRef={ref} filename="blog.pdf">
+          {({ toPdf }) => <button className="px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-700" onClick={toPdf}>Generate Pdf</button>}
+        </Pdf>
       </div>
-      <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
-        <h1 className="my-5 text-2xl font-semibold">How to validate React props using PropTypes?</h1>
-        <p className="font-normal">
-        PropTypes is a tool in React that helps ensure that the correct types of props are being passed into components. To use PropTypes, we define a propType for each prop in our component using the prop-types library. This helps prevent errors and make our code more reliable.
-        </p>
+      <div ref={ref} className="my-container lg:w-2/4">
+        <div className="p-8 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
+          <h1 className="my-5 text-2xl font-semibold">
+            Differences between uncontrolled and controlled components
+          </h1>
+          <p className="font-normal">
+            Controlled components use React state to manage and update their
+            values, while uncontrolled components rely on the browser's default
+            behavior and the DOM API. Controlled components offer more control
+            and flexibility, while uncontrolled components are simpler to set
+            up.
+          </p>
+        </div>
+        <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
+          <h1 className="my-5 text-2xl font-semibold">
+            How to validate React props using PropTypes?
+          </h1>
+          <p className="font-normal">
+            PropTypes is a tool in React that helps ensure that the correct
+            types of props are being passed into components. To use PropTypes,
+            we define a propType for each prop in our component using the
+            prop-types library. This helps prevent errors and make our code more
+            reliable.
+          </p>
+        </div>
+        <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
+          <h1 className="my-5 text-2xl font-semibold">
+            Difference between nodejs and express js?
+          </h1>
+          <p className="font-normal">
+            Node.js is a runtime environment that executes JavaScript code on
+            the server. Express.js is a framework built on top of Node.js that
+            provides features and tools for building web applications.
+            Express.js makes it easier to create server-side applications with
+            Node.js.
+          </p>
+        </div>
+        <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
+          <h1 className="my-5 text-2xl font-semibold">
+            What is custom hook, and why will you create a custom hook?
+          </h1>
+          <p className="font-normal">
+            A custom hook is a JavaScript function that allows you to reuse
+            logic across your React components. You might create a custom hook
+            to simplify my code, remove duplication, or make my components more
+            modular and reusable.
+          </p>
+        </div>
       </div>
-      <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
-        <h1 className="my-5 text-2xl font-semibold">Difference between nodejs and express js?</h1>
-        <p className="font-normal">
-        Node.js is a runtime environment that executes JavaScript code on the server. Express.js is a framework built on top of Node.js that provides features and tools for building web applications. Express.js makes it easier to create server-side applications with Node.js.
-        </p>
-      </div>
-      <div className="p-8 mt-6 rounded-lg bg-gradient-to-r from-indigo-50 to-purple-50">
-        <h1 className="my-5 text-2xl font-semibold">What is custom hook, and why will you create a custom hook?</h1>
-        <p className="font-normal">
-        A custom hook is a JavaScript function that allows you to reuse logic across your React components. You might create a custom hook to simplify my code, remove duplication, or make my components more modular and reusable.
-        </p>
-      </div>
-    </div>
-    );
+    </section>
+  );
 };
 
 export default Blog;
